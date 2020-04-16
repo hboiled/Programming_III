900ebde0ff99dea43adf928df7f1296dc66a91a9
Sam Lee <sflee36@yahoo.com.au>
2020-04-16 20:22:29 +0800

    a6 added

---
+++ A6/JMCClimateSensor/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace JMCClimateSensor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.howToUseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.weatherTable = new System.Windows.Forms.DataGridView();
+            this.Monday = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tuesday = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Wednesday = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Thursday = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Friday = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Saturday = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sunday = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CurrentView = new System.Windows.Forms.Label();
+            this.CurrentMonth = new System.Windows.Forms.Label();
+            this.BtnClear = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.weatherTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(798, 36);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.newToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(54, 32);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(176, 34);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(176, 34);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(176, 34);
+            this.saveToolStripMenuItem.Text = "Save As";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(176, 34);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.howToUseToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(65, 32);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // howToUseToolStripMenuItem
+            // 
+            this.howToUseToolStripMenuItem.Name = "howToUseToolStripMenuItem";
+            this.howToUseToolStripMenuItem.Size = new System.Drawing.Size(205, 34);
+            this.howToUseToolStripMenuItem.Text = "How to use";
+            this.howToUseToolStripMenuItem.Click += new System.EventHandler(this.howToUseToolStripMenuItem_Click);
+            // 
+            // weatherTable
+            // 
+            this.weatherTable.AllowUserToAddRows = false;
+            this.weatherTable.AllowUserToResizeColumns = false;
+            this.weatherTable.AllowUserToResizeRows = false;
+            this.weatherTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.weatherTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Monday,
+            this.Tuesday,
+            this.Wednesday,
+            this.Thursday,
+            this.Friday,
+            this.Saturday,
+            this.Sunday});
+            this.weatherTable.Location = new System.Drawing.Point(43, 126);
+            this.weatherTable.Name = "weatherTable";
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.Format = "N2";
+            dataGridViewCellStyle1.NullValue = "0";
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.weatherTable.RowHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.weatherTable.RowHeadersWidth = 62;
+            this.weatherTable.RowTemplate.Height = 28;
+            this.weatherTable.Size = new System.Drawing.Size(670, 350);
+            this.weatherTable.TabIndex = 1;
+            this.weatherTable.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.weatherTable_EditingControlShowing);
+            // 
+            // Monday
+            // 
+            this.Monday.HeaderText = "Mon";
+            this.Monday.MinimumWidth = 8;
+            this.Monday.Name = "Monday";
+            this.Monday.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Monday.Width = 50;
+            // 
+            // Tuesday
+            // 
+            this.Tuesday.HeaderText = "Tue";
+            this.Tuesday.MinimumWidth = 8;
+            this.Tuesday.Name = "Tuesday";
+            this.Tuesday.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Tuesday.Width = 50;
+            // 
+            // Wednesday
+            // 
+            this.Wednesday.HeaderText = "Wed";
+            this.Wednesday.MinimumWidth = 8;
+            this.Wednesday.Name = "Wednesday";
+            this.Wednesday.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Wednesday.Width = 50;
+            // 
+            // Thursday
+            // 
+            this.Thursday.HeaderText = "Thu";
+            this.Thursday.MinimumWidth = 8;
+            this.Thursday.Name = "Thursday";
+            this.Thursday.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Thursday.Width = 50;
+            // 
+            // Friday
+            // 
+            this.Friday.HeaderText = "Fri";
+            this.Friday.MinimumWidth = 8;
+            this.Friday.Name = "Friday";
+            this.Friday.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Friday.Width = 50;
+            // 
+            // Saturday
+            // 
+            this.Saturday.HeaderText = "Sat";
+            this.Saturday.MinimumWidth = 8;
+            this.Saturday.Name = "Saturday";
+            this.Saturday.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Saturday.Width = 50;
+            // 
+            // Sunday
+            // 
+            this.Sunday.HeaderText = "Sun";
+            this.Sunday.MinimumWidth = 8;
+            this.Sunday.Name = "Sunday";
+            this.Sunday.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Sunday.Width = 50;
+            // 
+            // CurrentView
+            // 
+            this.CurrentView.AutoSize = true;
+            this.CurrentView.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CurrentView.Location = new System.Drawing.Point(38, 82);
+            this.CurrentView.Name = "CurrentView";
+            this.CurrentView.Size = new System.Drawing.Size(209, 29);
+            this.CurrentView.TabIndex = 2;
+            this.CurrentView.Text = "Month Selected: ";
+            // 
+            // CurrentMonth
+            // 
+            this.CurrentMonth.AutoSize = true;
+            this.CurrentMonth.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CurrentMonth.Location = new System.Drawing.Point(248, 82);
+            this.CurrentMonth.Name = "CurrentMonth";
+            this.CurrentMonth.Size = new System.Drawing.Size(0, 29);
+            this.CurrentMonth.TabIndex = 3;
+            // 
+            // BtnClear
+            // 
+            this.BtnClear.Location = new System.Drawing.Point(608, 79);
+            this.BtnClear.Name = "BtnClear";
+            this.BtnClear.Size = new System.Drawing.Size(88, 32);
+            this.BtnClear.TabIndex = 4;
+            this.BtnClear.Text = "Clear";
+            this.BtnClear.UseVisualStyleBackColor = true;
+            this.BtnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 494);
+            this.Controls.Add(this.BtnClear);
+            this.Controls.Add(this.CurrentMonth);
+            this.Controls.Add(this.CurrentView);
+            this.Controls.Add(this.weatherTable);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Climate Sensor Reader";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.weatherTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.DataGridView weatherTable;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label CurrentView;
+        private System.Windows.Forms.Label CurrentMonth;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Monday;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tuesday;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Wednesday;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Thursday;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Friday;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Saturday;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sunday;
+        private System.Windows.Forms.Button BtnClear;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem howToUseToolStripMenuItem;
+    }
+}
+
---
+++ A6/JMCClimateSensor/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LumenWorks.Framework.IO.Csv;
+
+namespace JMCClimateSensor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // create a blank table which accepts user input
+        // table is 7 days by 6 weeks to allow a potential max range
+        // for a 31 day month starting on sunday
+        private void InitBlankForm()
+        {
+            ClearAll();
+
+            int daysPerWeek = 7;
+            int weeksPerMonth = 6;
+            weatherTable.TopLeftHeaderCell.Value = "Values in °C";
+
+            for (int i = 0; i < weeksPerMonth; i++)
+            {
+                string[] emptyArr = new string[daysPerWeek];                
+                for (int j = 0; j < daysPerWeek; j++)
+                {
+                    emptyArr[j] = "";
+                }
+                weatherTable.Rows.Add(emptyArr);
+                FillHeadRows(i);
+            }
+        }
+
+        // reader method which populates table based on values from a csv file
+        private void InsertValuesToTable(string path)
+        {
+            ClearAll();
+            //string path = "../../weatherData/march.csv";
+            using (CsvReader csv = new CsvReader(
+                                   new StreamReader(path), false))
+            {
+                int fieldCount = csv.FieldCount;
+                //***DEBUG if this is missing, throws missing field exception
+                //***this action ensures all records for that row aren't corrupt
+                csv.MissingFieldAction = MissingFieldAction.ReplaceByNull;
+                int headIndex = 0;
+                while (csv.ReadNextRecord()) 
+                {
+                    string[] row = new string[fieldCount];
+                    
+                    for (int i = 0 ; i < fieldCount; i++)
+                    {
+                        row[i] = csv[i] == "" ? "NA" : csv[i];
+                    }
+                    weatherTable.Rows.Add(row);
+                    FillHeadRows(headIndex);
+                    headIndex++;
+                }
+            }
+        }
+
+        // name and identify row headers according to week
+        private void FillHeadRows(int index)
+        {
+            weatherTable.Rows[index].HeaderCell.Value = "Week " + (index + 1);
+            weatherTable.AutoResizeRowHeadersWidth(index, 
+                DataGridViewRowHeadersWidthSizeMode.AutoSizeToDisplayedHeaders);
+            
+        }
+
+
+        // deletes all rows
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            ClearAll();
+        }
+
+        // delete all rows, to prevent excess row generation
+        private void ClearAll()
+        {
+            CurrentMonth.Text = "None";
+            weatherTable.Rows.Clear();
+            weatherTable.Refresh();
+        }
+
+        // disable user from entering inappropriate chars
+        private void weatherTable_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            // cast to textbox allows greater control and to set restrictions
+            TextBox tb = e.Control as TextBox;
+            tb.MaxLength = 5;
+            if (tb != null)
+            {
+                tb.KeyPress += new KeyPressEventHandler(Col_KeyPress);
+            }
+        }
+
+        private void Col_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // allow number, backspace and dot
+            if (!(char.IsDigit(e.KeyChar) || e.KeyChar == (char)Keys.Back || e.KeyChar == '.'))
+            {
+                e.Handled = true;
+
+            }
+            // allow only one dot
+            if (e.KeyChar == '.' && (sender as TextBox).Text.Contains("."))
+            {
+                e.Handled = true;
+
+            }
+        }
+
+        // create open file dialog and to get a filepath then load it to table
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {            
+            string filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = Path.Combine(
+                    Path.GetDirectoryName(Application.ExecutablePath), @"../../weatherData");
+                openFileDialog.AutoUpgradeEnabled = true;                
+                openFileDialog.Filter = "All files(*.*)| *.*|csv files (*.csv)|*.csv";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // get file path for other operations
+                    filePath = openFileDialog.FileName;
+                    InsertValuesToTable(filePath);
+                    SetMonthYearDisplay(filePath);
+                }                
+            }
+
+            
+        }
+
+        // open save file dialog to save files as .csv
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.InitialDirectory = Path.Combine(
+                    Path.GetDirectoryName(Application.ExecutablePath), @"weatherData");
+                saveFileDialog.Filter = "All files(*.*)| *.*|csv files (*.csv)|*.csv";
+                saveFileDialog.FilterIndex = 2;
+                saveFileDialog.RestoreDirectory = true;
+                
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = saveFileDialog.FileName;
+                    SaveFile(filePath);
+                    SetMonthYearDisplay(filePath);
+                                        
+                }
+            }
+        }
+
+        // save method which takes all values within dgv and saves as a csv
+        private void SaveFile(string file)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(file))
+                {
+                    foreach (DataGridViewRow row in weatherTable.Rows)
+                    {
+                        int colCount = weatherTable.Columns.Count;
+                        for (int i = 0; i < colCount; i++)
+                        {
+                            if (i != (colCount - 1))
+                            {
+                                sw.Write(row.Cells[i].Value + ",");
+                            }
+                            else
+                            {
+                                sw.Write(row.Cells[i].Value + "\n");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                MessageBox.Show("Could not save file - " + e.Message);
+            }
+            
+        }
+
+        // create new blank form
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ClearAll();
+            InitBlankForm();
+        }
+
+        // exit
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        // basic instructions
+        private void howToUseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //set title
+            MessageBox.Show(
+                "File -> Open -> .csv file of your choice >> to read and edit" +
+                "\n\n" +
+                "To add or edit, click any cell and input the temperature. " +
+                "Only digits and one decimal point may be entered. " +
+                "For any days which lie outside the month, leave them blank " +
+                "and they will be automatically be marked NA when loaded.",
+                "Instructions"
+                );
+        }
+
+        // Set date display (in month/year) of selected csv file
+        private void SetMonthYearDisplay(string filePath)
+        {
+            string displayName = Path.GetFileNameWithoutExtension(filePath).
+                Replace('_', ' ').ToUpper();
+            CurrentMonth.Text = displayName;
+        }
+    }
+}
+
